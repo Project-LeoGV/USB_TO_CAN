09272631e5dd7d2d4fcb3315b79731e8eed37f6f
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2023-11-28 23:52:24 +0200

    Transmission start

--- CAN/CAN_Interface.h
+++ CAN/CAN_Interface.h
@@ -13,6 +13,25 @@
 #include "CAN_Config.h"
 #include "CAN_Private.h"
 
+typedef struct{
+	u32 id;		// Identifier
+	u8 dlc;		// Data length code
+	u8 rtr;		// Remote transmission request
+	u8 ide; 	// Identifier extension
+	u8 data[8];
+}CAN_Frame_t;
+
+/* RTR(Remote transmission request) */
+#define CAN_DATA_FRAME		0
+#define CAN_REMOTE_FRAME	1
+
+/* IDE(Identifier extension) */
+#define CAN_STANDARD_IDENTIFIER		0
+#define CAN_EXTENDED_IDENTIFIER		1
+
+
 void CAN_voidInit(st_CAN_RegDef_t* canx, CAN_Config_t* config);
+
+void CAN_voidSendDataFrame(st_CAN_RegDef_t* canx, CAN_Frame_t* frame);
 
 #endif /* CAN_CAN_INTERFACE_H_ */
--- CAN/CAN_Program.c
+++ CAN/CAN_Program.c
@@ -50,3 +50,41 @@ void CAN_voidInit(st_CAN_RegDef_t* canx, CAN_Config_t* config)
 	// Wait for entering Normal Mode
 	while(  ((canx->MSR & (1<<0))>>0) );
 }
+
+void CAN_voidSendDataFrame(st_CAN_RegDef_t* canx, CAN_Frame_t* frame)
+{
+	/* Set the ID */
+	if(frame->ide == CAN_STANDARD_IDENTIFIER){
+		canx->TI0R &= ~(1 << 2);
+		canx->TI0R &= ~(0b11111111111 << 21); 	// Clear the previous id
+		canx->TI0R |= (frame->id << 21);		// Set the new id
+	}
+	else{
+		canx->TI0R |= (1<<2);
+		canx->TI0R &= ~(0xFFFFFFF8 << 3); 		// Clear the previous id
+		canx->TI0R |= (frame->id << 3);			// Set the new id
+	}
+
+	/* Set The DLC(Data Length Code) */
+	canx->TDT0R &= ~(0b1111 << 0);
+	canx->TDT0R |= (frame->dlc << 0);
+
+	/* Set The Data */
+	for(u8 i = 0; i < frame->dlc; i++)
+	{
+		if(i < 4){
+			canx->TDL0R &= ~(0xFF << i * 8);
+			canx->TDL0R |= (frame->data[i] << i * 8);
+		}
+		else{
+			canx->TDH0R &= ~(0xFF << (i - 4) * 8);
+			canx->TDH0R |= (frame->data[i] << (i - 4) * 8);
+		}
+	}
+
+	/* Request Transmission */
+	canx->TI0R |= (1 << 0);
+
+	while( !((canx->TSR & (1<<0))>>1) );	// Wait for Successful Transmission
+
+}
