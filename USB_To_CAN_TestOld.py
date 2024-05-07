f632f2cc28df2517d364e4d1c4ba1b5a768f93fe
mahmoudahmedibrahim5 <mahmoudahmedibrahim5@gmail.com>
2024-05-07 16:41:40 +0300

    Working

---
+++ USB_To_CAN_TestOld.py
@@ -0,0 +1,64 @@
+import serial
+
+def sendCan():
+    # Take input topic name and data,
+    # and convert it to CAN message
+
+    # Send it via USB
+    # ser.write(value.encode('ascii'))
+    print("Enter ID consists of three bits\n")
+    ID   = input("ID: 0x")
+    RTR  = input("RTR: ")
+    DLC  = input("DLC: ")
+    DATA =""
+    for i in range(1,int(DLC)+1,1):
+        Data = input("Data[{}]: ".format(i))
+        DATA += Data
+
+    msg = ID +RTR +DLC +DATA 
+    msg = msg.ljust(13,'0') + '\n'
+    print(msg)
+    ser.write(msg.encode('ascii'))
+    
+
+def receiveCan():
+    # Take can Message comes from USB,
+    # and convert it to topic name and data
+
+    # print it in the terminal
+    # ser.readline().decode()
+  
+    # msg = ser.readline().decode()
+    msg = ser.readline()
+    print(msg)
+    #print("********* Received CAN Message *********")
+    #print("ID = 0x" + msg[0:3])
+    #print("RTR = " + msg[3])
+    #print("DLC = " + msg[4])
+    #print("Data[8] = {" + msg[5], end="")
+    #for i in range(6, 13, 1):
+        #print(", " + msg[i], end="")
+    #print("}")
+
+if __name__ == "__main__":
+    print("Welcome To USB")
+    print("Enter The number of The port")
+    
+    com = "/dev/ttyACM0"     # Linux
+    #com = "com" + input()               # Windows
+
+    ser = serial.Serial(port=com, baudrate=115200)
+
+    while True:
+        ##################### Send CAN Message (DownStream) #####################
+        sendCan()
+        
+        ############# Receive Ack and message if required (UpStream) ############
+        Ack = ser.readline().decode()
+        if Ack == "0001\n":
+            print("Sending...")
+            continue
+
+        elif Ack == "0002\n":
+            print("Recciving...")
+            receiveCan()
